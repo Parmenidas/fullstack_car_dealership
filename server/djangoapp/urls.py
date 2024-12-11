db3d91248193180fd34ea0901b9ec6d73c086771
Your Name <you@example.com>
2024-12-11 11:46:06 -0500

    Add car listing

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -13,6 +13,9 @@ urlpatterns = [
 
     # path for logout
     path(route='logout', view=views.logout_request, name='logout'),
+
+    # Path to get list of cars
+    path(route='get_cars', view=views.get_cars, name ='getcars'),
     
 
     # path for dealer reviews view
